9cf8c1f0b56c04cad2d2b60ee63a444e61fe2872
youhei usami <usao9110@icloud.com>
2026-03-21 12:28:11 +0400

    3/21

--- js/script.js
+++ js/script.js
@@ -4,27 +4,25 @@ $(function () {
     $(".js-drawer").fadeToggle();
   });
 
-// ヘッダーの高さ分だけコンテンツを下げる
-$(function () {
-  const height = $(".js-header").height();
-  $("main").css("margin-top", height);
-});
+  // ヘッダーの高さ分だけコンテンツを下げる
+  $(function () {
+    const height = $(".js-header").height();
+    $("main").css("margin-top", height);
+  });
 
-$(function () {
+  $(function () {
+    const headerHeight = $(".js-header").height();
+    $('a[href^="#"]').click(function () {
+      const speed = 600;
+      let href = $(this).attr("href");
+      let target = $(href == "#" || href == "" ? "html" : href);
 
-  const headerHeight = $(".js-header").height();
-  $('a[href^="#"]').click(function () {
-    const speed = 600;
-    let href = $(this).attr("href");
-    let target = $(href == "#" || href == "" ? "html" : href);
-   
-    let position = target.offset().top - headerHeight;
-    $("body,html").animate({ scrollTop: position }, speed, "swing");
-    return false;
+      let position = target.offset().top - headerHeight;
+      $("body,html").animate({ scrollTop: position }, speed, "swing");
+      return false;
+    });
   });
 });
-});
-
 
 const params = new URLSearchParams(window.location.search);
 
@@ -32,4 +30,8 @@ document.getElementById("confirm-name").textContent = params.get("name");
 document.getElementById("confirm-mail").textContent = params.get("mail");
 document.getElementById("confirm-tel").textContent = params.get("tel");
 document.getElementById("confirm-address").textContent = params.get("contents");
-document.getElementById("confirm-type").textContent = params.get("type");
+document.getElementById("confirm-type").textContent = params.get("inquiryType");
+document.getElementById("confirm-quantity").textContent =
+  params.get("quantity");
+document.getElementById("confirm-wrap").textContent = params.get("wrap");
+document.getElementById("confirm-message").textContent = params.get("message");
